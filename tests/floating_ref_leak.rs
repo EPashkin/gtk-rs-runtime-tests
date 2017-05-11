82bfeb48a2cbb18ffdc7d1b7b3cf488396f49638
Evgenii Pashkin <eapashkin@gmail.com>
2017-05-11 18:36:55 +0300

    Add test for floating reference

---
+++ tests/floating_ref_leak.rs
@@ -0,0 +1,35 @@
+extern crate gtk;
+extern crate gtk_sys;
+extern crate glib;
+extern crate glib_sys;
+extern crate gobject_sys;
+
+use glib::translate::ToGlibPtr;
+
+#[test]
+fn floating_ref_leak() {
+    gtk::init().unwrap();
+
+    let ptr = {
+        let button = gtk::Button::new();
+        let ptr = get_pointer(&button);
+        assert_eq!(get_ref_count(ptr), 1);
+        let c = button.clone();
+        let ptr = get_pointer(&c);
+        assert_eq!(get_ref_count(ptr), 2);
+        ptr
+    };
+    //Warning: direct access to freed memory
+    assert_eq!(get_ref_count(ptr), 0);
+}
+
+fn get_pointer(btn: &gtk::Button) -> *const gobject_sys::GObject {
+    let ptr: *const gtk_sys::GtkButton = btn.to_glib_none().0;
+    ptr as *const gobject_sys::GObject
+}
+
+fn get_ref_count(obj: *const gobject_sys::GObject) -> u32 {
+    unsafe{
+        *(&(*obj).ref_count as *const glib_sys::Volatile<u32> as *const u32)
+    }
+}
